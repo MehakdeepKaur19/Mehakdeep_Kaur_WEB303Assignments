8cade7d9bf6915b68a3701823a4d4d73ac111568
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-09-24 02:16:17 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+    var content = $("#content");
+  
+    function loadContent(url) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
+  
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            content.fadeOut(400, function () {
+              content.html(xhr.responseText);
+              content.fadeIn(400);
+            });
+          } else {
+            console.log("Error loading content.");
+          }
+        }
+      };
+  
+      xhr.send();
+    }
+  
     // Attach click event handlers to the links
     $("#prospect").click(function () {
       loadContent("prospect.html");
@@ -11,27 +33,5 @@ $(document).ready(function () {
     $("#retain").click(function () {
       loadContent("retain.html");
     });
-  
-    function loadContent(url) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", url, true);
-      var content = $("content");
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            content.style.opacity = 0;
-  
-            setTimeout(function () {
-              content.innerHTML = xhr.responseText;
-              content.style.opacity = 1;
-            }, 400);
-          } else {
-            console.log("Error loading content.");
-          }
-        }
-      };
-  
-      xhr.send();
-    }
   });
   
