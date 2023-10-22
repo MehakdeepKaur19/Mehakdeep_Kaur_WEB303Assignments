2233b666eac7204f318523dd68f4db5a74ff89cf
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-10-22 17:01:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-/*
-    Assignment 05
-*/
-
 $(document).ready(function () {
-    class ContentItem{
-        contructor(id,name,des,category){
+    class ContentItem {
+        constructor(id, name, des, category) {
             this.id = id;
             this.name = name;
             this.des = des;
             this.category = category;
         }
-        updateContentItem(userId){
-            if(this.id === userId){
+        
+        updateContentItem(userId, name, des, category) {
+            if (this.id === userId) {
                 if (name !== null) {
                     this.name = name;
                 }
-               if (des !== null) {
-                this.des = des;
-            }
-            if (category !== null) {
-                this.category = category;
-            }
+                if (des !== null) {
+                    this.des = des;
+                }
+                if (category !== null) {
+                    this.category = category;
+                }
             }
         }
+        
         toString() {
             return `
-                <div class="content-item-itself" id="content-item-${this.id}">
+                <div id='content-item-${this.id}'>
                     <h2>${this.name}</h2>
                     <p>${this.des}</p>
                     <div>${this.category}</div>
@@ -33,7 +31,7 @@ $(document).ready(function () {
             `;
         }
     }
+    
     const item = new ContentItem(1, 'Item 1', 'Description 1', 'Category A');
     console.log(item.toString());
 });
-
