482416572c8e6ce547bd6208353c5c2755cf3562
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-10-29 00:49:26 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 $(document).ready(function() {
     // Accordion functionality
-    $('.accordion-label').click(function() {
-        var accordion = $(this).closest('.accordion');
-        accordion.find('.accordion-content').not($(this).next()).slideUp('fast');
-        $(this).next().slideToggle('fast');
+    $('.accord-label').on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $accordion = $this.next('.accord-panel');
+
+        // Toggle the showing class and slide the panel
+        $this.toggleClass('showing');
+        $accordion.slideToggle();
+
+        // Hide other panels
+        $('.accord-panel').not($accordion).slideUp();
+        $('.accord-label').not($this).removeClass('showing');
     });
 
-    // Tabbed section functionality
-    $('.tab').click(function() {
-        var tabId = $(this).data('tab');
-        $('.tab-content').hide();
-        $('#' + tabId).show();
+    // Tabbed Section functionality
+    $('.tab-panel').hide();
+    $('#tab1').show();
+
+    $('.tab-list li').on('click', function(e) {
+        e.preventDefault();
+
+        // Remove current active class
+        $('.tab-list li').removeClass('active');
+
+        // Hide current tab panel
+        $('.tab-panel').hide();
+
+        // Add active class to new tab
+        $(this).addClass('active');
+
+        // Show new tab panel
+        var panel = $(this).find('a').attr('href');
+        $(panel).show();
     });
 });
