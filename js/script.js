aee74a6a7243899ce8cf386cbff6756b100c186d
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-10-30 11:51:29 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
 //Event Handler
 $('.accord-label').on('click', function(e){
     e.preventDefault();
-    let $this = $(this);
-    $('.accord-panel').each(function()
-    {
-        $this.removeClass('showing');
-    });
 
+    let $this = $(this);
     $this.toggleClass('showing');
     $this.next().slideToggle();
-    $('.accord-panel').not($this.next()).slideUp();
 });
 
 //Tabbed Panels
 $('.tab-panel').hide();
 $('#tab1').show();
 
-$('.tab-list li').on('click', function(e)
-{
+$('.tab-list li').on('click', function(e){
     e.preventDefault();
+    
     $('.tab-list li').removeClass('active');
     $('.tab-panel').hide();
+    
     $(this).addClass('active');
     
     let panel = $(this).find('a').attr('href');
