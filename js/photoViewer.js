28d68ae27b161a9ef628300ea169a9278d3f0089
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-11-06 01:29:48 -0500

    Update photoViewer.js

--- js/photoViewer.js
+++ js/photoViewer.js
@@ -6,18 +6,32 @@
             let $thumbnails = $photoViewer.find('.thumbnail-anchor');
 
             // Handle click on a thumbnail
-            $thumbnails.click(function() {
-                let imgURL = $(this).attr('href');
-                $mainImage.attr('href', imgURL); // Set the href attribute to the clicked thumbnail's href
-                $mainImage.find('img').fadeOut(100, function() {
-                    $mainImage.find('img').attr('src', imgURL).fadeIn(100);
-                });
+            $thumbnails.on({
+                mouseover: function(){
+                    $(this).css({
+                        'cursor': 'pointer',
+                        'border-color': 'red'
+                    });
+                },
+                mouseout: function(){
+                    $(this).css({
+                        'cursor': 'default',
+                        'border-color': 'grey'
+                    });
+                },
+                click: function(){
+                    let imgURL = $(this).attr('href');
+                    $mainImage.attr('href', imgURL); // Set the href attribute to the clicked thumbnail's href
+                    $mainImage.find('img').fadeOut(100, function() {
+                        $mainImage.find('img').attr('src', imgURL).fadeIn(100);
+                    });
 
-                // Remove the "active" class from other thumbnails
-                $thumbnails.removeClass('active');
+                    // Remove the "active" class from other thumbnails
+                    $thumbnails.removeClass('active');
 
-                // Add the "active" class to the clicked thumbnail
-                $(this).addClass('active');
+                    // Add the "active" class to the clicked thumbnail
+                    $(this).addClass('active');
+                }
             });
         });
 
