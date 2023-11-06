1e3a519d8613acca5508a0640f3e9ff12a3ddff5
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-11-05 23:14:36 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -7,5 +7,28 @@ $(function () {
             height: 'auto'
         });
         //modal code goes here
+       $('#close').on('click',function(){
+           $('#box-container').hide();
+       });
+       $('#photo-box').on({
+        mouseover: function() {
+            $(this).css({
+                'cursor': 'pointer',
+                'border-color': 'red'
+            });
+        },
+        mouseout: function() {
+            $(this).css({
+                'cursor': 'default',
+                'border-color': 'grey'
+            });
+        },
+        click: function() {
+            let imgURL = $(this).attr('href'); // Get the href of the clicked thumbnail
+            $('#modal-image').fadeOut(100, function() {
+                $(this).attr('src', imgURL); // Update the src attribute with the clicked thumbnail's href
+            }).fadeIn(100);
+        }
+    });
     });
 });
