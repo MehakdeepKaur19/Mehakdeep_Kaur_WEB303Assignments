00d1cf9664b41c22b153be99bb5412c26f3a60d3
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-11-06 00:08:26 -0500

    changes

--- js/photoViewer.js
+++ js/photoViewer.js
@@ -1,38 +1,20 @@
-(function ($) {
-    $.fn.customPhotoViewer = function () {
-        // Function to open the modal and display the full-sized image
-        function openModal(thumbnail) {
-            const modal = $('#box-container');
-            const modalImage = $('#modal-image');
-
-            const fullImageSrc = thumbnail.attr('href');
-
-            // Set the href of a.photo-box to the clicked thumbnail's href
-            $('a.photo-box').attr('href', fullImageSrc);
-
-            modalImage.attr('src', fullImageSrc);
-
-            modal.show();
-        }
-
-        // Bind a click event to the thumbnails for opening the modal
-        this.on('click', '.thumbnail-anchor', function (e) {
-            e.preventDefault(); // Prevent the default link behavior
-
-            const clickedThumbnail = $(this);
-            openModal(clickedThumbnail);
+$('.photo-thumbnails').on({
+    mouseover: function(){
+        $(this).css({
+            'cursor':'pointer',
+            'border-color':'red'
+    });
+    },
+    mouseout:function(){
+        $(this).css({
+            'cursor':'default',
+            'border-color':'grey'
         });
-
-        // Function to close the modal
-        function closeModal() {
-            $('#box-container').hide();
-        }
-
-        // Bind a click event to the close button for closing the modal
-        $('#close-images').on('click', function () {
-            closeModal();
-        });
-
-        return this; // Enable method chaining
-    };
-})(jQuery);
+    },
+    click:function(){
+        let imgURL = $(this).attr('src');
+        $('.thumbnail-anchor active').fadeOut(100,function(){
+            $(this).attr('src', imgURL);
+        }).fadeIn(100);
+    }
+});
