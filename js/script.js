d8ff311ae62141e55c8eb256cde72a07e5e04fdc
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-11-05 23:18:45 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,29 +6,46 @@ $(function () {
             width: '100%',
             height: 'auto'
         });
-        //modal code goes here
-       $('#close').on('click',function(){
-           $('#box-container').hide();
-       });
-       $('#photo-box').on({
-        mouseover: function() {
+        
+        // Open the modal when a thumbnail is clicked
+        openModal($content);
+    });
+    
+    // Close the modal when the "Close" element is clicked
+    $('#close').on('click', function () {
+        closeModal();
+    });
+    
+    // Photo viewer functionality
+    $('.photo-box').on({
+        mouseover: function () {
             $(this).css({
                 'cursor': 'pointer',
                 'border-color': 'red'
             });
         },
-        mouseout: function() {
+        mouseout: function () {
             $(this).css({
                 'cursor': 'default',
                 'border-color': 'grey'
             });
         },
-        click: function() {
-            let imgURL = $(this).attr('href'); // Get the href of the clicked thumbnail
-            $('#modal-image').fadeOut(100, function() {
-                $(this).attr('src', imgURL); // Update the src attribute with the clicked thumbnail's href
-            }).fadeIn(100);
+        click: function () {
+            var imgURL = $(this).attr('href');
+            openModal($('<img src="' + imgURL + '" alt="Full-Sized Image" />'));
         }
     });
-    });
+    
+    // Function to open the modal and display the content
+    function openModal(content) {
+        var modal = $('#box-container');
+        var modalImage = $('#modal-image');
+        modalImage.html(content);
+        modal.show();
+    }
+    
+    // Function to close the modal
+    function closeModal() {
+        $('#box-container').hide();
+    }
 });
