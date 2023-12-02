912afdeb4756fe31077b09cff952ae277322e01d
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-12-02 16:29:01 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,13 +32,8 @@ $(document).ready(function () {
     // Event listener for form submission
     $('#registration').on('submit', function (event) {
         const username = $('#username').val();
-        const selectedCountry = $('#country :selected').text();
-
-        // Check if all fields are filled before redirecting
-        if (username && selectedCountry) {
-            // Redirect to registration.html with parameters
-            window.location.href = `register.html?username=${encodeURIComponent(username)}&country=${encodeURIComponent(selectedCountry)}`;
-        }
+        const selectedCountry = $('#country :selected').val();
+		$('#welcomeMessage').append('<p>Welcome'+$username+'! The country code you selected is'+selectedCountry+'</p>');
 
         event.preventDefault(); // Prevent form from submitting for demo purposes
     });
