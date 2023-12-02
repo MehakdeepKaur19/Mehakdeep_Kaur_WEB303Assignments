f535a38a5807b5fab14d01d1a65bb8170f232f95
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-12-02 16:11:44 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 $(document).ready(function () {
+    const countryDropdown = $('#country');
+
+    // Populate the country dropdown dynamically from countries.js
+    countries.forEach(function (countryName) {
+        const option = $('<option></option>');
+        option.val(countryName);  // You can use country code as the value if needed
+        option.text(countryName);
+        countryDropdown.append(option);
+    });
+
     // Function to check if all form requirements are met
     function checkFormRequirements() {
         const username = $('#username').val();
         const password = $('#password').val();
         const confirmPassword = $('#checkpsd').val();
         const termsCheckbox = $('#terms').prop('checked');
-		const countryDropdown = $('#country');
-
-		// Populate the country dropdown dynamically from countries.js
-		countries.forEach(function (countryName) {
-			const option = $('<option></option>');
-			option.val(countryName);  // You can use country code as the value if needed
-			option.text(countryName);
-			countryDropdown.append(option);
-		});
+        const selectedCountry = $('#country').val();
 
         const submitButton = $('#submitButton');
 
         // Enable the submit button if all requirements are met
-        submitButton.prop('disabled', !(username && password.length >= 12 && confirmPassword === password && termsCheckbox && country));
+        submitButton.prop('disabled', !(username && password.length >= 12 && confirmPassword === password && termsCheckbox && selectedCountry));
     }
 
     // Event listeners to check form requirements on input/change
