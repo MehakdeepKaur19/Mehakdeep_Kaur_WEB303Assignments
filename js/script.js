6c0b5e064cf248e1becb0177b66e7776926debd5
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-12-02 16:07:07 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -1,24 +1,14 @@
 $(document).ready(function () {
-    // Populate the country dropdown dynamically
-    var countryDropdown = $('#country');
-
-    countries.forEach(function (countryName) {
-        var option = $('<option></option>');
-        option.val(countryName.substring(0, 2).toUpperCase()); // Using first two characters as country code
-        option.text(countryName);
-        countryDropdown.append(option);
-    });
-
     // Function to check if all form requirements are met
     function checkFormRequirements() {
-        var username = $('#username').val();
-        var password = $('#password').val();
-        var confirmPassword = $('#confirmPassword').val();
-        var termsCheckbox = $('#termsCheckbox').prop('checked');
-        var country = $('#country').val();
+        const username = $('#username').val();
+        const password = $('#password').val();
+        const confirmPassword = $('#checkpsd').val();
+        const termsCheckbox = $('#terms').prop('checked');
+        const country = $('#country').val();
 
-        var submitButton = $('#submitButton');
-        
+        const submitButton = $('#submitButton');
+
         // Enable the submit button if all requirements are met
         submitButton.prop('disabled', !(username && password.length >= 12 && confirmPassword === password && termsCheckbox && country));
     }
@@ -26,20 +16,18 @@ $(document).ready(function () {
     // Event listeners to check form requirements on input/change
     $('#username').on('input', checkFormRequirements);
     $('#password').on('input', checkFormRequirements);
-    $('#confirmPassword').on('input', checkFormRequirements);
-    $('#termsCheckbox').on('change', checkFormRequirements);
+    $('#checkpsd').on('input', checkFormRequirements);
+    $('#terms').on('change', checkFormRequirements);
     $('#country').on('change', checkFormRequirements);
 
     // Event listener for form submission
-    $('#registrationForm').on('submit', function (event) {
-        event.preventDefault(); // Prevent form from redirecting to /register
+    $('#registration').on('submit', function (event) {
+        event.preventDefault(); // Prevent form from submitting for demo purposes
 
-        var welcomeMessage = $('#welcomeMessage');
-        var username = $('#username').val();
-        var selectedCountry = $('#country').val();
+        const username = $('#username').val();
+        const selectedCountry = $('#country :selected').text(); // Get the selected country text
 
-        // Display welcome message
-        welcomeMessage.text('Welcome ' + username + '! The country code you selected is ' + selectedCountry);
-        welcomeMessage.css('display', 'block');
+        // Redirect to registration.html with parameters
+        window.location.href = `registration.html?username=${encodeURIComponent(username)}&country=${encodeURIComponent(selectedCountry)}`;
     });
 });
