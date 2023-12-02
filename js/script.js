7f42e80117b8fdbe9227eb0e13bda3c6e4269182
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-12-02 15:59:30 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,119 +1,45 @@
-//do not forget to enter document.ready function
-//also jquery library should be included
+$(document).ready(function () {
+    // Populate the country dropdown dynamically
+    var countryDropdown = $('#country');
 
+    countries.forEach(function (countryName) {
+        var option = $('<option></option>');
+        option.val(countryName.substring(0, 2).toUpperCase()); // Using first two characters as country code
+        option.text(countryName);
+        countryDropdown.append(option);
+    });
 
-//adding radio buttons for the courses
-let h2 = document.querySelector('h2');
-for (let i = 0; i < countries.length; i++) {
-  let check = document.createElement('input');
-  check.setAttribute('type', 'checkbox');
-  check.name = 'checked';
-  check.id = countries[i];
-  check.value = countries[i];
-  let lable = document.createElement('label');
-  lable.setAttribute('for', countries[i]);
-  lable.innerHTML = countries[i];
-  h2.after(lable);
-  lable.appendChild(check);
+    // Function to check if all form requirements are met
+    function checkFormRequirements() {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var confirmPassword = $('#confirmPassword').val();
+        var termsCheckbox = $('#termsCheckbox').prop('checked');
+        var country = $('#country').val();
 
-}
-
-//We are going to write a listener for the checked buttons
-//getting the name and one of the radio buttons
-//will create a message
-
-let form = document.querySelector('#registration');
-let subBtn = document.querySelector('#submit');
-let parMsg = document.createElement('p');
-let elements = form.elements;
-
-subBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  if (validation()) {
-    alert('This is a demp. No form is submitted');
-  }
-  let user = elements.username.value;
-  let checkboxes = document.getElementsByName('checked');
-  let ok = false;
-  let code;
-  for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      ok = true;
-      code = checkboxes[i].value;
+        var submitButton = $('#submitButton');
+        
+        // Enable the submit button if all requirements are met
+        submitButton.prop('disabled', !(username && password.length >= 12 && confirmPassword === password && termsCheckbox && country));
     }
-  }
-  let msg = `Welcome ${user}!The country code you selected is  ${code} `;
-  parMsg.textContent = msg;
-  form.appendChild(parMsg);
+
+    // Event listeners to check form requirements on input/change
+    $('#username').on('input', checkFormRequirements);
+    $('#password').on('input', checkFormRequirements);
+    $('#confirmPassword').on('input', checkFormRequirements);
+    $('#termsCheckbox').on('change', checkFormRequirements);
+    $('#country').on('change', checkFormRequirements);
+
+    // Event listener for form submission
+    $('#registrationForm').on('submit', function (event) {
+        event.preventDefault(); // Prevent form from redirecting to /register
+
+        var welcomeMessage = $('#welcomeMessage');
+        var username = $('#username').val();
+        var selectedCountry = $('#country').val();
+
+        // Display welcome message
+        welcomeMessage.text('Welcome ' + username + '! The country code you selected is ' + selectedCountry);
+        welcomeMessage.css('display', 'block');
+    });
 });
-
-//form validation
-
-let username = document.getElementById('username');
-let pass = document.getElementById('password');
-let repass = document.getElementById('checkpsd');
-
-const validation = function () {
-  if (fname.value == '') {
-    alert("Please enter your username");
-    fname.focus();
-    return false;
-
-  }
-
-  if (pass.value == '' || pass.value.length <= 12) {
-    alert("Please enter your password and it should be more than 12 characters");
-    pass.focus();
-    return false;
-  }
-
-  if (repass.value == '' || pass.value != repass.value) {
-    alert("Please enter your password and it should be more than 12 characters");
-    pass.focus();
-    return false;
-  }
-
-  let checkboxes = document.getElementsByName('checked');
-  let ok = false;
-  let code;
-  for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      ok = true;
-      code = checkboxes[i].value;
-    }
-  }
-  if (!ok) {
-    alert("Please check on box");
-    return false;
-  }
-
-
-  return true;
-}
-
-//we need our button to be disabled until the form is validated
-//validation will happen witth any input
-
-subBtn.classList.remove('enabled');
-subBtn.ariaDisabled = true;
-subBtn.disabled = true;
-subBtn.classList.add('disabled');
-
-let inputs = document.querySelectorAll('#registration input');
-
-for (let i = 0; i < inputs.length; i++) {
-  let checkValid = function () {
-    if (!validation()) {
-      subBtn.disabled = true;
-      subBtn.classList.add('disabled');
-    }
-    else {
-      subBtn.disabled = false;
-      subBtn.classList.remove('disabled');
-      alert('Demo Only. No form is submitted');
-    }
-  }
-  inputs[i].addEventListener('input', checkValid
-  );
-}
