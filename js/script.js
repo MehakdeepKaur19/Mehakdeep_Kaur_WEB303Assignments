5a9cc2316c58ab496c432c54533eb3f256836168
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-12-02 16:33:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    const countryDropdown = $('#country');
-    const submitButton = $('#submitButton');
+    let countryDropdown = $('#country');
+    let submitButton = $('#submitButton');
 
     // Populate the country dropdown dynamically from countries.js
     countries.forEach(function (country) {
-        const option = $('<option></option>');
+        let option = $('<option></option>');
         option.val(country.code);
         option.text(country.name);
         countryDropdown.append(option);
@@ -12,11 +12,11 @@ $(document).ready(function () {
 
     // Function to check if all form requirements are met
     function checkFormRequirements() {
-        const username = $('#username').val();
-        const password = $('#password').val();
-        const confirmPassword = $('#checkpsd').val();
-        const termsCheckbox = $('#terms').prop('checked');
-        const selectedCountry = $('#country').val();
+        let username = $('#username').val();
+        let password = $('#password').val();
+        let confirmPassword = $('#checkpsd').val();
+        let termsCheckbox = $('#terms').prop('checked');
+        let selectedCountry = $('#country').val();
 
         // Enable the submit button if all fields are filled
         submitButton.prop('disabled', (username && password.length >= 12 && confirmPassword === password && termsCheckbox && selectedCountry));
@@ -31,9 +31,9 @@ $(document).ready(function () {
 
     // Event listener for form submission
     $('#registration').on('submit', function (event) {
-        const username = $('#username').val();
-        const selectedCountry = $('#country :selected').val();
-		$('#welcomeMessage').append('<p>Welcome'+$username+'! The country code you selected is'+selectedCountry+'</p>');
+        let username = $('#username').val();
+        let selectedCountry = $('#country :selected').val();
+		$('#welcomeMessage').append('<p>Welcome'+username+'! The country code you selected is'+selectedCountry+'</p>');
 
         event.preventDefault(); // Prevent form from submitting for demo purposes
     });
