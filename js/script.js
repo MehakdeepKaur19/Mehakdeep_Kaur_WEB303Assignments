1fe4cece5236d8d45a660248eb9a7ea3195ff9e4
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-12-03 00:00:03 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,76 +1,33 @@
 $(document).ready(function () {
-    // Populate the country dropdown dynamically from countries.js
-    for (let i = 0; i < countries.length; i++) {
-        $('#country').append('<option value="' + countries[i].code + '">' + countries[i].name + '</option>');
-    }
+    let countryDropdown = document.getElementById('country');
+            countries.forEach(function (country) {
+                let option = document.createElement('option');
+                option.value = country.code;
+                option.text = country.name;
+                countryDropdown.appendChild(option);
+            });
 
-    let form = document.querySelector('#registration');
-    let subBtn = document.getElementsByName('submit')[0]; // Use getElementsByName and select the first element
+            let form = document.getElementById('registration');
+            let submitButton = document.getElementById('submit');
+            let welcomeMessage = document.getElementById('welcomeMessage');
 
-    let parMsg = document.createElement('p');
+            function checkFormRequirements() {
+                let username = document.getElementById('username').value;
+                let password = document.getElementById('password').value;
+                let confirmPassword = document.getElementById('confirmPassword').value;
+                let termsCheckbox = document.getElementById('terms').checked;
+                let selectedCountry = countryDropdown.value;
 
-    subBtn.onclick = function (e) {
-        e.preventDefault();
-        let user = form.elements.username.value; // Corrected from uname to username
-        let selectedCountry = form.elements.country.value;
+                submitButton.disabled = (username && password.length >= 12 && confirmPassword === password && termsCheckbox && selectedCountry);
+            }
 
-        let msg = `Welcome ${user}! The country code you selected is ${selectedCountry}.`;
-        parMsg.textContent = msg;
-        form.appendChild(parMsg); // Corrected from append to appendChild
-    };
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
+                let username = document.getElementById('username').value;
+                let selectedCountry = countryDropdown.options[countryDropdown.selectedIndex].text;
 
-    let uname = form.elements.username;
-    let pass = form.elements.password;
-    let repass = form.elements.checkpsd;
+                welcomeMessage.innerHTML = `<p>Welcome ${username}! The country code you selected is ${selectedCountry}</p>`;
+            });
 
-    const validation = function () {
-        if (uname.value === '') {
-            alert("Please enter your username");
-            uname.focus();
-            return false;
-        }
-
-        if (pass.value === '' || pass.value.length < 5) {
-            alert("Please enter a valid password larger than 5 characters");
-            pass.focus();
-            return false;
-        }
-
-        if (pass.value === '' || pass.value !== repass.value) {
-            alert("Please re-enter your password same as before");
-            repass.focus();
-            return false;
-        }
-
-        let selectedCountry = form.elements.country.value;
-
-        if (!selectedCountry) {
-            alert('Please select a country from the dropdown list');
-            return false;
-        }
-
-        return true;
-    };
-
-    subBtn.classList.remove('enabled');
-subBtn.disabled = true;
-subBtn.classList.add('disabled');
-
-let inputs = document.querySelectorAll('#registration input');
-
-for(let i = 0; i<inputs.length; i++) {
-    let checkValid = function(){
-        if(!validation()){
-            subBtn.disabled = true;
-            subBtn.classList.add('disabled');
-        }
-        else {
-            subBtn.disabled = false;
-            subBtn.classList.remove('disabled');
-            alert('This is a Demo, No Form is submited');
-        }
-
-    }
-    inputs[i].addEventListener('change', checkValid);
-}
-});
+            form.addEventListener('input', checkFormRequirements);
+        });
