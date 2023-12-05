2c761fd1b212e871b8422c9fc21a915b636944b3
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2023-12-05 10:09:04 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,75 +1,118 @@
-$(document).ready(function () {
-    let countryDropdown = document.getElementById('country');
+document.addEventListener("DOMContentLoaded", function () {
+    // Creating the form and its elements 
+  
+    let form = document.createElement("form");
+    form.id = "registrationForm";
+  
+    let usernameLabel = createLabel("Username:");
+    let usernameInput = createInput("text", "username", true);
+  
+    let passwordLabel = createLabel("Password (Must be at least 12 characters):");
+    let passwordInput = createInput("password", "password", false);
+  
+    let password2Label = createLabel("Re-enter your Password:");
+    let password2Input = createInput("password", "password2", false);
+    let passwordMatchMessage = document.createElement("span");
+    passwordMatchMessage.id = "passwordMatchMessage";
+  
+    let termsLabel = createLabel("I accept the terms of service:");
+    let termsInput = createInput("checkbox", "terms", false, "yes");
+  
+    let countryLabel = createLabel("Select your country:");
+    let countrySelect = document.createElement("select");
+    countrySelect.id = "country";
+    countrySelect.name = "country";
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.text = "Select your country";
+    countrySelect.appendChild(defaultOption);
     countries.forEach(function (country) {
-        let option = document.createElement('option');
-        option.value = country.code;
-        option.text = country.name;
-        countryDropdown.appendChild(option);
+      const option = document.createElement("option");
+      option.value = country.code;
+      option.text = country.name;
+      countrySelect.appendChild(option);
     });
-
-    let form = document.getElementById('registration');
-    let subBtn = document.getElementById('submit');
-    let welcomeMessage = document.getElementById('welcomeMessage');
-    let username = document.getElementById('username');
-    let password = document.getElementById('password');
-    let selectedCountry = countryDropdown.options[countryDropdown.selectedIndex].value;
-    let confirmPassword = document.getElementById('confirmPassword');
-    let termsCheckbox = document.getElementById('terms');
-
-    const validation = function () {
-        if (username.value == '') {
-            alert("Please enter username");
-            username.focus();
-            return false;
-        }
-        if (password.value == '' || password.value.length < 12) {
-            alert("Please enter a valid password larger than 12 letters");
-            password.focus();
-            return false;
-        }
-        if (password.value == '' || password.value != confirmPassword.value) {
-            alert("Please re-enter your password same as before");
-            confirmPassword.focus();
-            return false;
-        }
-        if (!termsCheckbox.checked) {
-            alert("You must agree to the Terms and Conditions.");
-            termsCheckbox.focus();
-            return false;
-        }
-        if (selectedCountry === '') { // Fix: Check if selectedCountry is empty
-            alert("Please select the country");
-            countryDropdown.focus(); // Fix: Set focus on countryDropdown
-            return false;
-        }
-        return true;
-    };
-
-    subBtn.classList.remove('enabled');
-    subBtn.disabled = true;
-    subBtn.classList.add('disabled');
-
-    let inputs = document.querySelectorAll('#registration input');
-
-    for (let i = 0; i < inputs.length; i++) {
-        let checkValid = function () {
-            if (!validation()) {
-                subBtn.disabled = true;
-                subBtn.classList.add('disabled');
-            } else {
-                subBtn.disabled = false;
-                subBtn.classList.remove('disabled');
-                subBtn.classList.add('enabled');
-            }
-        };
-        inputs[i].addEventListener('change', checkValid);
+  
+    let submitButton = createInput("submit", "submit", true, "Submit");
+    submitButton.disabled = true;
+  
+    let welcomeMessage = document.createElement("p");
+    welcomeMessage.id = "welcomeMessage";
+    welcomeMessage.style.display = "none";
+  
+    // Append elements to the form
+    form.appendChild(usernameLabel);
+    form.appendChild(usernameInput);
+    form.appendChild(passwordLabel);
+    form.appendChild(passwordInput);
+    form.appendChild(password2Label);
+    form.appendChild(password2Input);
+    form.appendChild(passwordMatchMessage);
+    form.appendChild(termsLabel);
+    form.appendChild(termsInput);
+    form.appendChild(countryLabel);
+    form.appendChild(countrySelect);
+    form.appendChild(submitButton);
+    form.appendChild(welcomeMessage);
+  
+    // Append form to the body
+    document.body.appendChild(form);
+  
+    // Event listeners for form controls
+    usernameInput.addEventListener("input", validateForm);
+    passwordInput.addEventListener("input", validateForm);
+    password2Input.addEventListener("input", validateForm);
+    termsInput.addEventListener("change", validateForm);
+    countrySelect.addEventListener("change", validateForm);
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      // Display welcome message with the country code
+      var selectedCountryCode = countrySelect.value;
+      welcomeMessage.innerText = `Welcome ${usernameInput.value}! The country code you selected is ${selectedCountryCode}.`;
+      welcomeMessage.style.display = "block";
+  
+    });
+  
+    // Function to create label
+    function createLabel(text) {
+      let label = document.createElement("label");
+      label.htmlFor = text.toLowerCase().replace(/\s/g, "");
+      label.innerText = text;
+      return label;
     }
-
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        let username = document.getElementById('username').value;
-        let selectedCountry = countryDropdown.options[countryDropdown.selectedIndex].value;
-
-        welcomeMessage.innerHTML = `<p>Welcome ${username}! The country code you selected is ${selectedCountry}</p>`;
-    });
-});
+  
+    // Function to create input element
+    function createInput(type, id, required, value) {
+      let input = document.createElement("input");
+      input.type = type;
+      input.id = id;
+      input.name = id;
+      if (required) {
+        input.required = true;
+      }
+      if (value) {
+        input.value = value;
+      }
+      return input;
+    }
+  
+    // Function to validate the form and enable/disable the submit button
+    function validateForm() {
+      let isUsernameValid = usernameInput.value.trim() !== "";
+      let isPasswordValid = passwordInput.value.length >= 12;
+      let isPasswordMatch = passwordInput.value === password2Input.value;
+      let isTermsChecked = termsInput.checked;
+      let isCountrySelected = countrySelect.value !== "";
+  
+      submitButton.disabled = !(isUsernameValid && isPasswordValid && isPasswordMatch && isTermsChecked && isCountrySelected);
+  
+      // Display password match message
+      if (!isPasswordMatch) {
+        passwordMatchMessage.innerText = "Passwords do not match!";
+      } else {
+        passwordMatchMessage.innerText = "";
+      }
+    }
+  });
